58fc7bdc8c02e7c2f234e70625d03dfb3142b758
Dellix <40401562+Dellix@users.noreply.github.com>
2018-06-21 20:21:19 +0200

    Update dbConnect.php

--- php/dbConnect.php
+++ php/dbConnect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', '<db_user>', '<db_password>');
+$mysqli = new mysqli('localhost', '<db_user>', '<db_password>', 'pi');
     if ($mysqli->connect_error) {
         die('Connection Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);}
 ?>
